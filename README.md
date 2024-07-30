e279ea3651c15a3647f5a8336fc94dbc881261bc
alpaca <pinetree2100@gmail.com>
2024-07-30 05:20:49 +0000

    add vscode

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# vimrc
+## Developement Setup Automation
+### vimrc
 ---
 Make sure that you're using vim, not a vi editor.  
 Download [Vundle](https://github.com/VundleVim/Vundle.vim) as a vim package manager.  
@@ -6,3 +7,8 @@ Packages used in [.vimrc](https://github.com/alanskye/vimrc/blob/main/.vimrc):
 1. [Nerdtree](https://github.com/preservim/nerdtree): filesystem explorer in the Vim editor
 2. [lightline](https://github.com/itchyny/lightline.vim): status line plugin
 3. [gruvbox](https://vimawesome.com/plugin/gruvbox): my favorite colorscheme
+
+### visual studio code extension
+---
+Modified from [vscode-extension-install-automation](https://github.com/mugglim vscode-extension-install-automation/tree/main)  
+Add vscode identifers you want to install to the txt file. You can find the 'Unique Identifier' in vscode marketplace website
---
+++ setup_vscode/vscode_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname "$0")
+EXTENSION_IDENTIFIERS=$(cat "${BASE_DIR}/vscode-extension.txt")
+
+for identifier in $EXTENSION_IDENTIFIERS
+do
+    echo "[Installing $identifier]"
+    echo $(code --install-extension $identifier)
+done
+
+# $0: This represents the name of the script being executed. 
+# Depending on how the script is called, 
+# this could be a relative or absolute path.
+
+# dirname "$0": The dirname command is used to extract 
+# the directory path from a full file path. 
+# By passing $0 to dirname, it extracts the directory path portion of the script's location.
+
+
+# original script written by @mugglim
+# https://github.com/mugglim/vscode-extension-install-automation/tree/main
+
+# Add vscode identifers you want to install to the txt file.
+# You can find the 'Unique Identifier' in the vscode marketplace website
