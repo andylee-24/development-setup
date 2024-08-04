1d4760a31c12fcbc95e0b337b96004fcab0086e6
alpaca <pinetree2100@gmail.com>
2024-08-04 05:55:35 +0000

    add zsh setups

---
+++ setup_zsh/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "set-option -g default-shell /bin/zsh" >> ~/.tmux.conf
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Ignore update message
+echo "DISABLE_AUTO_UPDATE=\"true\"" >> ~/.zshrc
+
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
