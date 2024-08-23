a1ed98d90e37bdb04643c9667dfe0d7a4eafecb7
TaeHan Lee <45648726+andylee-24@users.noreply.github.com>
2024-08-23 15:38:21 +0900

    disable update prompt

--- setup_zsh/run.sh
+++ setup_zsh/run.sh
@@ -3,7 +3,7 @@ echo "set-option -g default-shell /bin/zsh" >> ~/.tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Ignore update message
 echo "DISABLE_AUTO_UPDATE=\"true\"" >> ~/.zshrc
-
+echo "DISABLE_UPDATE_PROMPT=true" >> ~/.zshrc
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
