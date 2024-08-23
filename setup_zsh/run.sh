f12abb3d59fe705b6130c63d6011022f9906428f
TaeHan Lee <45648726+andylee-24@users.noreply.github.com>
2024-08-23 15:44:36 +0900

    Update run.sh

--- setup_zsh/run.sh
+++ setup_zsh/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "set-option -g default-shell /bin/zsh" >> ~/.tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Ignore update message
-echo "DISABLE_AUTO_UPDATE=\"true\"" >> ~/.zshrc
-echo "DISABLE_UPDATE_PROMPT=true" >> ~/.zshrc
+
+# Ignore update message (should be placed before calling source $ZSH/oh-my-zsh.sh)
+echo "export DISABLE_UPDATE_PROMPT=true" | cat - ~/.zshrc > temptemptemp && mv temptemptemp ~/.zshrc
+echo "export DISABLE_AUTO_UPDATE=true" | cat - ~/.zshrc > temptemptemp && mv temptemptemp ~/.zshrc
+
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
